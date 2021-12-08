195b69a27b79fdfd335dbe49dbf9b7760a546205
Raihan Kabir <raihancse46@gmail.com>
2021-12-08 11:25:11 +0600

    member ship page 90% done

--- assets/js/common.js
+++ assets/js/common.js
@@ -7,15 +7,21 @@ $(document).ready(function () {
   })
 
 
-
+  // small device navbar collapse
   $('.country_list.sm-device, .main_navbar .icon_group .close_icon').on('click', function () {
     $('.country_list_collapse').collapse('toggle');
     $('.icon_group').toggleClass('show_close')
   })
 
   $('.hamberger_icon').on('click', function () {
-    $('.navbar-collapse').collapse('toggle');
+    $('.small_device_navbar').collapse('toggle');
     $('.country_list.sm-device').toggleClass('events_none');
+  })
+
+  // small device submenu collapse
+  $('.sub_menu_btn').on('click', function () {
+    $(this).parent().children('.sub_menu.collapse, .country_list_submenu').collapse('toggle');
+    $(this).toggleClass('icon_rotate');
   })
 
 
